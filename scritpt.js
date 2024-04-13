291b2f4cabe10551e67ace339c751fb4e46fa9a5
alluringxstalwart <alluringxstalwart@gmail.com>
2024-04-13 18:15:40 +0530

    add scroll ability

--- scritpt.js
+++ scritpt.js
@@ -1,16 +1,33 @@
-window.onload = function(){    
-    document.getElementById("menu").onclick = function(){
+window.onload = function () {
+    document.getElementById("menu").onclick = function () {
         document.getElementById("aside").style.display = 'flex';
     }
 
-    
 
-    document.getElementById("cross").onclick = function(){
+
+    document.getElementById("cross").onclick = function () {
         document.getElementsByClassName("aside")[0].style.display = "none";
     }
 
-    document.getElementById("contact").onclick = function(){
+    document.getElementById("contact").onclick = function () {
         window.open('mailto:alluringxstalwart@gmail.com')
     }
+    document.getElementById('homebutton').addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // smooth scrolling
+        });
+    });
+
+    document.getElementById("projectsbutton").addEventListener('click', function () {
+        // Scroll to the top of the projects section
+        document.getElementById("projects").scrollIntoView({ behavior: 'smooth', block: 'start' });
+    });
+
+    document.getElementById("blogsbutton").addEventListener('click', function () {
+        // Scroll to the top of the blogs section
+        document.getElementById("blogs").scrollIntoView({ behavior: 'smooth', block: 'start' });
+    });
+
 }
 
